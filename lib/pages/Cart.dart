9f89583889d1798c21d24a5f33463b59db1c219a
Ryan Hirasaki <ryanhirasaki@gmail.com>
2020-11-20 11:58:30 -0600

    Convert Cart  to Stateful

--- lib/pages/Cart.dart
+++ lib/pages/Cart.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 
-class Cart extends StatelessWidget {
+class Cart extends StatefulWidget {
   static const String ROUTE = "/cart";
+  @override
+  State<StatefulWidget> createState() {
+    return _CartState();
+  }
+}
 
+class _CartState extends State<Cart> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
