097d7b9fc74d89bb5f868f2698e8b89af6af6810
Colin Wong <cwongmath@gmail.com>
2021-11-10 00:45:03 -0600

    Small tweaks, prep for merge

--- lib/consts.dart
+++ lib/consts.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 const String TITLE = "K'Ching MarketPlace Online";
-const String TITLE_SHORT = "KMP Online";
+const String TITLE_SHORT = "K'Ching MP Online";
 const String ENDPOINT =
     // "https://cors-anywhere.herokuapp.com/" + 
-    "https://script.google.com/macros/s/AKfycbwmnd3LbyB4kMGmMV9VNBnLvgKioWUjHt2ys9dq5uL52VXuks-TXZRsJvFupJI0QB7R-w/exec";
+    "https://script.google.com/macros/s/AKfycbzfJ4qHX0oUA5lgXVWI_t2W_DFWylnrrItvkjWIwmiEfV3MFC47JOZFR6NAmi6gtvh8nA/exec";
 
 const double STORES_HEIGHT = 70;
 const double STORES_WIDTH = 250;
@@ -12,7 +12,7 @@ const double STORES_WIDTH = 250;
 const MaterialColor PRIMARY_COLOR = Colors.lightGreen;
 
 const String ERROR_LOADING_PRODUCTS =
-    "Error Loading Products, try again later!\nIf you think this is an error, report to facilitators.";
+    "Error loading products, try again later!\nIf you think this is an error, report to facilitators.";
 
 const String KCHING_BUCK_SYM = "₭\$";
 
--- lib/utils/api.dart
+++ lib/utils/api.dart
@@ -2,7 +2,6 @@ import 'dart:convert' as convert;
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:pos_system/consts.dart';
-import 'dart:developer';
 
 enum Tasks { Login, Teams, Products, Purchase }
 
@@ -25,27 +24,15 @@ extension TaskAPI on Tasks {
   }
 }
 
-String _buildGetHeader(Map<String, String> data) {
-  String buf = "";
-  data.forEach((key, value) {
-    buf += key + "=" + value + "&";
-    // buf += "$key=$value&";
-  });
-  if (buf.length != 0) {
-    buf = buf.substring(0, buf.length - 1); // Remove final &
-  }
-  return buf;
-}
-
 Future<Map<String, dynamic>> getter(
     Tasks task, Map<String, String> data) async {
-  String _processedData = _buildGetHeader(data);
+  String _processedData = data.entries.map((e) => e.key + "=" + e.value).toList().join("&");
   String _task = task.api;
   final response = await http.get("$ENDPOINT?task=$_task&$_processedData");
   print("$ENDPOINT?task=$_task&$_processedData");
   print((await http.get("$ENDPOINT?task=sheetName")).body);
   assert(response.statusCode == 200);
-  log(response.body);
+  print(response.body);
   return convert.json.decode(response.body);
 }
 
