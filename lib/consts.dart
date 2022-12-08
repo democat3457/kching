a35bdd8d47bb5f22e283d8b9af487cfbe324119a
Colin Wong <cwongmath@gmail.com>
2022-12-08 08:49:37 -0600

    Update endpoint

--- lib/consts.dart
+++ lib/consts.dart
@@ -6,8 +6,8 @@ import 'package:flutter/material.dart';
 const String TITLE = "K'Ching MarketPlace Online";
 const String TITLE_SHORT = "K'Ching MP Online";
 const String ENDPOINT =
-    // "https://cors-anywhere.herokuapp.com/" + 
-    "https://script.google.com/macros/s/AKfycbxBLlFdr_Ds4FyfS40Ge8O4zBOYQA50l3aBwfvj_pHm1QgFF8FGFtQSRzEDFT2fD8Nj8g/exec";
+    // "https://cors-anywhere.herokuapp.com/" +
+    "https://script.google.com/macros/s/AKfycbyTkaBKhg7rB0yjG6Ae9CUpsYAyQRrSwqPA8o_man9lRg0Gpz35jAt7HJG8FuHiDyIp3A/exec";
 
 const double STORES_HEIGHT = 60;
 const double STORES_WIDTH = 250;
@@ -23,9 +23,11 @@ const String KCHING_BUCK_SYM = "₭\$";
 
 const double PADDING = 24;
 
-final DateTime OPEN_TIME  = DateTime.utc(2022, 12, 3, 9, 0); // devtime: 2022/12/03 3:00PM
+final DateTime OPEN_TIME =
+    DateTime.utc(2022, 12, 3, 9, 0); // devtime: 2022/12/03 3:00PM
 // final DateTime OPEN_TIME  = DateTime.utc(2022, 12, 8, 23, 0); // 2022/12/08 5:00PM
-final DateTime CLOSE_TIME = DateTime.utc(2022, 12, 9, 2, 10); // 2022/12/08 8:10PM
+final DateTime CLOSE_TIME =
+    DateTime.utc(2022, 12, 9, 2, 10); // 2022/12/08 8:10PM
 final DateFormat FORMATTER = DateFormat('HH:mm:ss');
 
 const bool DEV_MODE = false;
